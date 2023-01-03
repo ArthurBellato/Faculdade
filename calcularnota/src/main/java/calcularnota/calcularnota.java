b2ddbad575c4cf0342524c33d1e21602b32aa745
Arthur Bellato <104738136+ArthurBellato@users.noreply.github.com>
2023-01-03 20:00:30 -0300

    Add files via upload

---
+++ calcularnota/src/main/java/calcularnota/calcularnota.java
@@ -0,0 +1,11 @@
+package calcularnota;
+
+    
+public class calcularnota {    
+    
+        public static void main (String[] args){
+            janelafrontal obj = new janelafrontal();
+            obj.show();
+        }
+    
+}
